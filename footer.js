b423670f96c93a65f06384b7102ba2b5f6702c91
credactor <175741800+credactor@users.noreply.github.com>
2024-08-03 14:41:01 +0300

    Pages done

---
+++ footer.js
@@ -0,0 +1,19 @@
+document.write('\
+<footer class="page-footer grey darken-1">\
+<div class="container">\
+  <div class="row">\
+    <div class="col s12 l6">\
+      <h6>BanksInfo</h6>\
+      <p>This information website is created for olim hadashim as a part of portfolio when studying at <a href="https://developers.institute">Developers.Institute</a>, Ramat Gan</p>\
+    </div>\
+    <div class="col s12 l4 offset-l2">\
+      <h6>Made by</h6>\
+      <ul>\
+        <li>&copy; 2024 Daria</li>\
+        <li>&copy; 2024 Stanislav</li>\
+      </ul>\
+    </div>\
+  </div>\
+</div>\
+</footer>\
+');
